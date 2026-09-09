58b8e967de0c67fe2c5d04651288b681b979a0f8
luis_ <perghormaru@gmail.com>
2026-09-08 19:44:15 -0500

    feat: add supplier-id

---
+++ src/shared/domain/model/supplier-id.js
@@ -0,0 +1,29 @@
+import {generateUuid, validateUuid} from "./uuid.js";
+import {ValidationError} from "./errors.js";
+
+export class SupplierId {
+    #value;
+
+    constructor(value) {
+        if (!validateUuid(value))
+            throw new ValidationError(`Invalid supplier id: ${value}. Must be a valid UUID.`);
+        this.#value = value;
+        Object.freeze(this);
+    }
+
+    static generate() {
+        return new SupplierId(generateUuid());
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    equals(other) {
+        return other instanceof SupplierId && this.#value === other.value;
+    }
+
+    toString() {
+        return this.#value;
+    }
+}
