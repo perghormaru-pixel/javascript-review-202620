1a50d6560c9c2379feb09b3b01fade0877c01614
luis_ <perghormaru@gmail.com>
2026-09-08 19:44:43 -0500

    feat: add product-id.js

---
+++ src/shared/domain/model/product-id.js
@@ -0,0 +1,29 @@
+import {generateUuid, validateUuid} from "./uuid.js";
+import {ValidationError} from "./errors.js";
+
+export class ProductId {
+    #value;
+
+    constructor(value) {
+        if (!validateUuid(value))
+            throw new ValidationError(`Invalid product id: ${value}. Must be a valid UUID.`);
+        this.#value = value;
+        Object.freeze(this);
+    }
+
+    static generate() {
+        return new ProductId(generateUuid());
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    equals(other) {
+        return other instanceof ProductId && this.#value === other.value;
+    }
+
+    toString() {
+        return this.#value;
+    }
+}
